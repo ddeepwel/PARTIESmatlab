371672dc4c0feac901179c86f93d5b2ee9d0c5f4
David Deepwell <ddeepwel@uwaterloo.ca>
2021-01-11 15:14:13 -0800

    Reorganize and add reach bottom functions

---
+++ supplemental/reached_bottom.m
@@ -0,0 +1,29 @@
+function reach_bottom = reached_bottom(H)
+% check if particles have neared the bottom
+% in the simulation
+% returns true if any particle is closer to the bottom
+% than the distance H
+
+particle_files = dir('mobile_*.dat');
+N_files = length(particle_files);
+
+% height above bottom
+% when particle 'feels' the bottom
+if nargin == 0
+    H = 5;
+end
+
+% loop over all particles for minimum height
+height = zeros(1,N_files);
+for mm = 1:N_files
+    fname = sprintf('mobile_%d', mm-1);
+    p = check_read_dat(fname);
+    height(mm) = min(p.y);
+end
+
+if min(height) < H
+    reach_bottom = true;
+else
+    reach_bottom = false;
+end
+
